7ff4008bcb2f5fa1e1b2635610b0f37a52fd452d
savdmi <savdmien@yandex.ru>
2026-04-19 13:18:34 +0300

    feat(register) register service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,20 +8,33 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  Res,
+  Inject,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { CreateUserDto } from './../users/dto/create-user.dto';
+import { Response, Request, CookieOptions } from 'express';
+import { IJwtConfig, jwtConfig } from './../config/jwt.config';
+import ms, { StringValue } from 'ms';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfigService: IJwtConfig,
+  ) {}
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
-  async create(@Body() createAuthDto: CreateAuthDto): Promise<CreateUserDto> {
-    return await this.authService.create(createAuthDto);
+  async register(
+    @Body() createAuthDto: CreateAuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { user, tokens } = await this.authService.register(createAuthDto);
+    this.setAuthCookies(res, tokens);
+    return { user };
   }
 
   @Get()
@@ -42,5 +55,25 @@ export class AuthController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
+  }
+
+  private setAuthCookies(
+    res: Response,
+    tokens: { accessToken: string; refreshToken: string },
+  ) {
+    const cookieOptions: CookieOptions = {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+    };
+
+    res.cookie('accessToken', tokens.accessToken, {
+      ...cookieOptions,
+      maxAge: ms(this.jwtConfigService.accessExpiresIn as StringValue),
+    });
+    res.cookie('refreshToken', tokens.refreshToken, {
+      ...cookieOptions,
+      maxAge: ms(this.jwtConfigService.refreshExpiresIn as StringValue),
+    });
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { IJwtConfig, jwtConfig } from './../config/jwt.config';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import * as bcrypt from 'bcrypt';
@@ -6,14 +7,20 @@ import { CreateUserDto } from './../users/dto/create-user.dto';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { appConfig, IAppConfig } from './../config/app.config';
+import ms, { StringValue } from 'ms';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    @Inject(appConfig.KEY)
+    private readonly configService: IAppConfig,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfigService: IJwtConfig,
   ) {}
-  async create(createAuthDto: CreateAuthDto): Promise<CreateUserDto> {
+  async register(createAuthDto: CreateAuthDto) {
     const userExists = await this.userRepository.findOne({
       where: { email: createAuthDto.email },
     });
@@ -21,9 +28,21 @@ export class AuthService {
       throw new BadRequestException('User already exists');
     }
 
-    const password = await bcrypt.hash(createAuthDto.password, 10);
-    const user = this.userRepository.create({ ...createAuthDto, password });
-    return this.userRepository.save(user);
+    const hash = await bcrypt.hash(
+      createAuthDto.password,
+      this.configService.salt,
+    );
+    const user = this.userRepository.create({
+      ...createAuthDto,
+      password: hash,
+    });
+    const tokens = await this.generateTokens(user.id);
+    user.refreshToken = tokens.refreshToken;
+    await this.userRepository.save(user);
+    return {
+      user,
+      tokens,
+    };
   }
 
   findAll() {
@@ -41,5 +60,18 @@ export class AuthService {
   remove(id: number) {
     return `This action removes a #${id} auth`;
   }
-}
 
+  private async generateTokens(userId: string) {
+    const payload = { sub: userId };
+    return {
+      accessToken: this.jwtService.sign(payload, {
+        secret: this.jwtConfigService.accessSecret,
+        expiresIn: ms(this.jwtConfigService.accessExpiresIn as StringValue),
+      }),
+      refreshToken: this.jwtService.sign(payload, {
+        secret: this.jwtConfigService.refreshSecret,
+        expiresIn: ms(this.jwtConfigService.refreshExpiresIn as StringValue),
+      }),
+    };
+  }
+}
