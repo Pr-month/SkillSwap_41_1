bf7fb38878b4509705b854f364e771f30c53f329
Stepan <Stepashkabasketball@mail.ru>
2026-04-22 21:03:43 +0700

    feat: add GET /categories endpoint

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IsNull, Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
   create(createCategoryDto: CreateCategoryDto) {
     return 'This action adds a new category';
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll() {
+    const result = await this.categoryRepository.find({
+      where: {
+        parent: IsNull(),
+      },
+      relations: ['children'],
+    });
+
+    return result;
   }
 
   findOne(id: number) {
