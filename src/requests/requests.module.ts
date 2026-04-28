660cb83c8daf25425d0902a3244879ea0f861539
savdmi <savdmien@yandex.ru>
2026-04-28 10:25:22 +0300

    feat: request remove + fix enum

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { RequestsController } from './requests.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { Request } from './entities/request.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Request, User])],
   controllers: [RequestsController],
   providers: [RequestsService],
 })
