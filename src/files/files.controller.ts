c0d9f77dc2b26b2e01bbbd3a3a92a502b47aa96d
Stepan <Stepashkabasketball@mail.ru>
2026-05-06 21:09:25 +0700

    docs: add swagger for file upload

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -9,12 +9,16 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import type { Request } from 'express';
 import { multerConfig } from './multer.config';
 import { FilesService } from './files.service';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiUploadFile } from './swagger/files.swagger';
 
+@ApiTags('files')
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post()
+  @ApiUploadFile()
   @UseInterceptors(FileInterceptor('file', multerConfig))
   upload(@UploadedFile() file: Express.Multer.File, @Req() req: Request) {
     const result =
