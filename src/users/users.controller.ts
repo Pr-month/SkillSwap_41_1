7c20cba90d4a0b47f8bb547dfb8ce21a296a6e90
Pavel Savelev <9173912682pavel@gmail.com>
2026-05-07 10:49:51 +0300

    add users swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,39 +14,55 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { IRequestWithUser } from 'src/auth/auth.types';
 import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
+import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreateUser,
+  ApiDeleteUser,
+  ApiFindAllUsers,
+  ApiGetUser,
+  ApiGetUserById,
+  ApiUpdateUser,
+} from './swagger/users.swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiCreateUser()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiFindAllUsers()
   findAll() {
     return this.usersService.findAll();
   }
 
   @UseGuards(AccessTokenGuard)
   @Get('me')
+  @ApiGetUser()
   getMe(@Req() req: IRequestWithUser) {
     return this.usersService.findOne(req.user.sub);
   }
 
   @Get(':id')
+  @ApiGetUserById()
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @UseGuards(AccessTokenGuard)
   @Patch('me')
+  @ApiUpdateUser()
   update(@Req() req: IRequestWithUser, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.updateMe(req.user.sub, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiDeleteUser()
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
